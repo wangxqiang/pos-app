0d71d010bee0570102e9103b7fa2e06f58c54f6f
user.name <729619377@qq.com>
2017-08-10 09:02:45 +0800

    第二次提交

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
-//选择/下载
 var app=null;
 var phone=null;
+var val;
+//判断是否微信登陆
+function isWeiXin() {
+    var ua = window.navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+        return true;
+    } else {
+        return false;
+    }
+}
+//判断设备
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+    val="ios";
+    console.log("苹果");
+    alert("ios");
+} else if (/(Android)/i.test(navigator.userAgent)) {
+    //android
+    alert("android");
+    if(isWeiXin()){
+        alert("微信内置浏览器");
+        $(".mb").css("display","block")
+    }else{
+        console.log("非微信内置浏览器");
+        $(".mb").css("display","none");
+        val="andriod";
+    }
+} else {
+    console.log("pc")
+};
+//选择/下载
 $(".posxz").click(function(){
-    $(".down").css('background-color',"rgba(255,0,0,.7)");
+    $(".down").css('background-color',"rgba(255,0,0,.5)");
     $(".posxz").css('background-color',"red");
     $(".posxz").html("已选择");
     $(".androidxz").css('background-color',"#C8C8A9");
@@ -11,7 +40,7 @@ $(".posxz").click(function(){
     keyup();
 });
 $(".androidxz").click(function(){
-    $(".down").css('background-color',"rgba(255,0,0,.7)");
+    $(".down").css('background-color',"rgba(255,0,0,.5)");
     $(".androidxz").css('background-color',"red");
     $(".androidxz").html("已选择");
     $(".posxz").css('background-color',"#C8C8A9");
@@ -21,31 +50,50 @@ $(".androidxz").click(function(){
 });
  function keyup(){
      var phone = $(".phone input").val();
-     var re = /^1\d{10}$/;
+     var re = /^1(3|4|5|7|8)\d{9}$/;
      if (re.test(phone) && phone!=""&&(app=="pos"||app=="andr")) {
          $(".down").css('background-color',"rgba(255,0,0,1)");
      }else{
-         $(".down").css('background-color',"rgba(255,0,0,.7)");
+         $(".down").css('background-color',"rgba(255,0,0,.5)");
+         if(phone.length==11&& !re.test(phone)){
+             $(".tishi").html("手机号错误");
+         }else{
+                 $(".tishi").html("");
+         }
      }
 }
 //选择下载完结
 $(".down").click(function(){
     var phone = $(".phone input").val();
-    var re = /^1\d{10}$/;
+    var re = /^1(3|4|5|7|8)\d{9}$/;
     if (re.test(phone) && phone!="") {
     }else{
+        $(".tishi").html("*手机号错误");
         return
     }
-
 if(app=="pos"||app=="andr"){
     $(".down").css('background-color',"red");
     if(app=="pos"){
-        console.log("去下载pos");
+        if(val=="ios"){
+            console.log("下载ios收银系统")
+            //location.href="";
+        }else if(val="android"){
+            console.log("下载Android收银系统")
+            //location.href="";
+        }
     }
     if(app=="andr"){
-        console.log("去下载手机端app");
+        if(val=="ios"){
+            console.log("下载ios APP后台管理")
+            //location.href="";
+        }else if(val="android"){
+            //location.href="";
+            console.log("下载android APP后台管理")
+        }
     }
 }else{
-console.log("请选择应用类型");
+alert("请选择应用类型");
 }
 })
+
+
